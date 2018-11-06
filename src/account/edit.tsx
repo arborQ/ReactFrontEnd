430543627cf8fe8fa9a94ff940883f9f4fb413de
Lukasz Wojcik (PGS Software) <lwojcik@pgs-soft.com>
2018-11-06 13:30:31 +0100

    dsadasd edit

--- src/account/edit.tsx
+++ src/account/edit.tsx
@@ -1,13 +1,68 @@
 import * as React from "react";
-import { Drawer, Skeleton } from "antd";
+import { Drawer, Skeleton, Form, Input } from "antd";
 import { RouteComponentProps } from "react-router";
+import { getUser } from "bx-services/users";
+interface IEditUserState {
+  loading: boolean;
+  userData: Areas.Account.IUser | null;
+}
 
-export default class Create extends React.PureComponent<RouteComponentProps> {
+export default class Create extends React.PureComponent<
+  RouteComponentProps<{ id: string }>,
+  IEditUserState
+> {
+  componentWillMount(): void {
+    this.setState({
+      loading: false,
+      userData: null
+    });
+  }
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      userData: null
+    });
+    getUser(+this.props.match.params.id).then(userData => {
+      this.setState({
+        ...this.state,
+        userData
+      });
+    });
+  }
   render() {
     return (
       <div>
-        <Drawer visible={true} title="Edit user" onClose={() => { this.props.history.goBack(); }}>
-          <Skeleton avatar paragraph={{ rows: 6 }} />
+        <Drawer
+          visible={true}
+          title="Edit user"
+          onClose={() => {
+            this.props.history.goBack();
+          }}
+        >
+          <Skeleton
+            loading={this.state.userData === null}
+            avatar
+            paragraph={{ rows: 6 }}
+          >
+            {this.state.userData === null ? null : (
+              <Form>
+                <Form.Item>
+                  <Input
+                    placeholder="Email address"
+                    value={this.state.userData.email}
+                    onChange={() => {}}
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Input
+                    placeholder="Login"
+                    value={this.state.userData.login}
+                    onChange={() => {}}
+                  />
+                </Form.Item>
+              </Form>
+            )}
+          </Skeleton>
         </Drawer>
       </div>
     );
--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -3,3 +3,8 @@ import { get } from "bx-utils/ajax";
 export function loadUsers(): Promise<Areas.Account.IUser[]> {
   return get("/api/account/users");
 }
+
+export function getUser(userId: number): Promise<Areas.Account.IUser> {
+    return get(`/api/account/users/${userId}`);
+  }
+  
